8bfbbf88966719e9d63d4d3156f24b1bb793960f
Mona Sun (CSI Interfusion Inc) <v-sunxi@microsoft.com>
2019-05-16 17:23:52 +0800

    update jdk12 case

---
+++ Myjdk12Test.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class Myjdk12Test {
+
+	@Test
+    public void test() {
+        switch (2) {
+            case 1 -> System.out.println("one");
+            case 2 -> System.out.println("two");
+            case 3 -> System.out.println("many");
+    }
+    // assertEquals(true, false);
+    }
+
+}
